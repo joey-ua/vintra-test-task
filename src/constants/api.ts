46469f6baf41fc9bf7e65dcd7a02cb0232878b12
Andrey Karpenko <karpuch13@yahoo.com>
2022-09-08 21:00:06 +0200

    add SENSORS_URL constant

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,3 +3,4 @@ export const THERMO_API_URL: string = process.env.REACT_APP_THERMO_API_URL ||
 
 export const AUTH_URL = `${THERMO_API_URL}/auth/login`;
 export const HEALTH_URL = `${THERMO_API_URL}/health`;
+export const SENSORS_URL = `${THERMO_API_URL}/api/v1/sensors/`;
