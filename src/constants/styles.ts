d8d82f0a7ad7b5d00e9783a060933a3a2b833068
Andrey Karpenko <karpuch13@yahoo.com>
2022-09-08 19:21:57 +0200

    add common styles constants

---
+++ src/constants/styles.ts
@@ -0,0 +1,9 @@
+// App layout
+export const PAGE_FOOTER =
+  'bg-white border-slate-200 border-t-2 p-6 text-center';
+export const PAGE_HEADER =
+  'bg-white border-b-2 border-slate-200 flex items-center justify-between p-6';
+
+// Buttons
+export const BUTTON_LINK_PRIMARY =
+  'cursor-pointer font-medium hover:text-slate-900 text-base text-slate-500 whitespace-nowrap';
