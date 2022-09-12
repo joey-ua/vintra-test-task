0bdbe514f61a5f7b2b8f6be759bbd5158955abac
Andrey Karpenko <karpuch13@yahoo.com>
2022-09-12 17:56:44 +0200

    update Dashboard with AddSensorButton

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -6,27 +6,38 @@ import useSWR from 'swr';
 import { useAuth } from '../../clientState/auth';
 
 // imports from components
+import AddSensorButton from '../../components/AddSensorButton/AddSensorButton';
 import Widget from '../../components/Widget/Widget';
 
 // imports from constants
 import { HEALTH_URL, SENSORS_URL } from '../../constants/api';
+
+// imports from utils
 import { fetchWithToken } from '../../utils/fetch';
 
 const Dashboard: FC = () => {
-  const { token } = useAuth();
+  const { destroyAuth } = useAuth();
   const health = useSWR(HEALTH_URL, { refreshInterval: 60000 });
-  const sensors = useSWR([token ? SENSORS_URL : null, token], fetchWithToken)
+  const sensors = useSWR(SENSORS_URL, fetchWithToken);
+
+  const data = sensors.data || [];
+
+  if (sensors.error) destroyAuth();
 
   if (!sensors.data) return null;
 
   return (
     <div className="p-4">
-      <h1 className="font-medium mb-4 py-4 text-xl">
-        Server status: { health.data?.db_status_ok ? 'Ok' : 'Down' }
-      </h1>
+      <div className="flex items-center justify-between">
+        <h1 className="font-medium mb-4 py-4 text-xl">
+          Server status: { health.data?.db_status_ok ? 'Ok' : 'Down' }
+        </h1>
+
+        <AddSensorButton sensors={sensors.data} />
+      </div>
 
       <div>
-        { sensors.data.map((item: any) => <Widget {...item} key={item.id} />) }
+        { data.map((item: any) => <Widget {...item} key={item.id} />) }
       </div>
     </div>
   );
