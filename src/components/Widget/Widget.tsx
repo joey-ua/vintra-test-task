bea14d4262d068a42fd474e6eb51fcda95ea929d
Andrey Karpenko <karpuch13@yahoo.com>
2022-09-12 17:57:08 +0200

    update Widget with DeleteSensorButton

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -1,20 +1,17 @@
 // imports from vendors
 import { FC } from 'react'
-import { MinusIcon } from '@heroicons/react/20/solid';
 
-type Props = {
-  isActive?: boolean
-  id: string
-  description: string
-}
+// imports from components
+import DeleteSensorButton from '../../components/DeleteSensorButton/DeleteSensorButton';
 
-const Widget: FC<Props> = ({ description, id, isActive = false }) => {
+// imports from types
+import { Sensor } from '../../types/sensor';
+
+const Widget: FC<Sensor> = ({ description, id, isActive = false }) => {
   const classNames = [
     'cursor-pointer',
     'flex',
     'focus:outline-none',
-    'hover:bg-indigo-600',
-    'hover:text-white',
     'mb-2',
     'p-6',
     'relative',
@@ -23,22 +20,20 @@ const Widget: FC<Props> = ({ description, id, isActive = false }) => {
     'shadow-md',
     'w-full',
     isActive ? 'bg-indigo-500 text-white' : 'bg-white',
-  ]
+  ];
 
   return (
-    <button className={classNames.join(' ')} onClick={() => console.log(id)}>
+    <div className={classNames.join(' ')}>
       <div className="flex items-center justify-between relative w-full">
         <div className="flex items-center">
           <div className="font-medium pr-10 text-sm">{ description }</div>
         </div>
-        { isActive && (
-          <div className="absolute bg-opacity-80 bg-indigo-400 h-8 p-1 right-0 rounded-full shrink-0 text-white w-8">
-            <MinusIcon className="h-6 w-6" />
-          </div>
-        ) }
+        <div className="flex">
+         <DeleteSensorButton id={id} />
+        </div>
       </div>
-    </button>
-  )
+    </div>
+  );
 };
 
 export default Widget;
